2e46adb1c5496d64f2b8b1462e80774165a29727
Jevgenij <fformula@gmail.com>
2018-07-25 16:41:59 +0300

    Sum

--- Program.java
+++ Program.java
@@ -3,5 +3,12 @@ public class Program
 	public static void main (String [] args)
 	{
 		System.out.println ("Hello Git!");
+		int s = sum (a, b);
+		System.out.println (s);
+	}
+
+	public static int sum (int a, int b)
+	{
+		return a + b;
 	}
 }
