d84adbbd1f14e47cdd07f190c8fa816d93be9e45
Jevgenij <fformula@gmail.com>
2018-07-25 18:20:46 +0300

    Class

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@ public class Program
 	public static void main (String [] args)
 	{
 		System.out.println ("Hello Git!");
-		int s = sum (a, b);
+		int s = sum (5, 7);
 		System.out.println (s);
 	}
 
