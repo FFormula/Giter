adcc8f63cea563321f5c09f3a910317a95739313
Jevgenij Volosatov <fformula@gmail.com>
2018-07-25 18:40:29 +0300

    271

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@ public class Program
 	public static void main (String [] args)
 	{
 		System.out.println ("Hello Git!");
-		int s = sum (25, 27);
+		int s = sum (25, 271);
 		System.out.println (s);
 	}
 
